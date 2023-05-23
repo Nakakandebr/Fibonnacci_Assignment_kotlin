a758f09cbdfb22958dd465ef27e9485b95292b41
Nakakandebr <bridgetnakakandea@gmail.com>
2023-05-23 19:45:07 +0300

    made changes

--- app/src/main/java/com/assignment/fibonacci_assignment/FibonnacciAdapter.kt
+++ app/src/main/java/com/assignment/fibonacci_assignment/FibonnacciAdapter.kt
@@ -1,28 +1,26 @@
-package com.assignment.fibonacci_assignment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.assignment.fibonacci_assignment.R
 
-class FibonacciAdapter(private val fibonacciNumbers: List<Int>) : RecyclerView.Adapter<FibonacciViewHolder>() {
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FibonacciViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.numbers_list, parent, false)
-        return FibonacciViewHolder(view)
+class FibonacciAdapter(private val data: List<Int>) : RecyclerView.Adapter<FibonacciAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FibonacciAdapter.ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.numbers_list, parent, false)
+        return ViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: FibonacciViewHolder, position: Int) {
-        holder.bind(fibonacciNumbers[position])
+    override fun onBindViewHolder(holder: FibonacciAdapter.ViewHolder, position: Int) {
+        holder.tvNumber.text = data[position].toString()
     }
 
     override fun getItemCount(): Int {
-        return fibonacciNumbers.size
+        return data.size
     }
-}
 
-class FibonacciViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-    fun bind(number: Int) {
-        itemView.fibonacci.int = number.toString()
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tvNumber: TextView = itemView.findViewById(R.id.tv_number)
     }
 }
